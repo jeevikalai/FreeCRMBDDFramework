e510e7209a4fcf10869c6bef270a346cd78ec9df
jeevikalai <jeevikalai@gmail.com>
2019-01-24 18:32:50 -0500

    Update TestRunner.java

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 	    features = "C:\\Users\\jeevi\\seleniumproject\\CucumberFramework\\src\\test\\java\\Feature\\login1.feature",
 	    glue="StepDefinition",
 	   
-	    dryRun = false, //to check every feature step has a step definition.
+	    dryRun = true, //to check every feature step has a step definition.
 	    monochrome = true, // makes readable console output
 	   strict = false, //fails if any pending or missed step definition found
  	format= {"pretty","html:test-output"})
